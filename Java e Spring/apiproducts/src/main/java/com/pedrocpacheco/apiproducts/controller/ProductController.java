1c5c87948f0c589dd4545f9b1df92f8c8f4d9b1e
pedrocpacheco <cpedropacheco@hotmail.com>
2023-05-04 23:51:17 -0300

    feat: adding a getDepartment method in ProductController

--- Java e Spring/apiproducts/src/main/java/com/pedrocpacheco/apiproducts/controller/ProductController.java
+++ Java e Spring/apiproducts/src/main/java/com/pedrocpacheco/apiproducts/controller/ProductController.java
@@ -1,8 +1,20 @@
 package com.pedrocpacheco.apiproducts.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pedrocpacheco.apiproducts.entities.Department;
+
 @RestController
+@RequestMapping(value = "/products")
 public class ProductController {
     
+    @GetMapping
+    public Department getDepartment(){
+        var dep = new Department(10L, "Pedro");
+
+        return dep;
+    }
+
 }
--- Java e Spring/apiproducts/src/main/java/com/pedrocpacheco/apiproducts/entities/Department.java
+++ Java e Spring/apiproducts/src/main/java/com/pedrocpacheco/apiproducts/entities/Department.java
@@ -4,17 +4,28 @@ public class Department {
     
     private Long id;
     private String name;
-    
+
+    public Department(Long id, String name) {
+        setId(id);
+        setName(name);
+    }
+
     public Long getId() {
         return id;
     }
     public void setId(Long id) {
+        if(id < 0){
+            throw new RuntimeException("ID Cannot be Null");
+        }
         this.id = id;
     }
     public String getName() {
         return name;
     }
     public void setName(String name) {
+        if(name.equals(null))
+            throw new RuntimeException("Name Cannot be Null");
+
         this.name = name;
     }
 
