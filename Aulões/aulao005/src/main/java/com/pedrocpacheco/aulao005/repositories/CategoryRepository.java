46331f471cbd7308c3f4f4753839c5603c7abf18
pedrocpacheco <cpedropacheco@hotmail.com>
2023-05-06 09:54:46 -0300

    feat(CategoryRepository): creating a save, getById, and getAll methods

---
+++ Aulões/aulao005/src/main/java/com/pedrocpacheco/aulao005/repositories/CategoryRepository.java
@@ -0,0 +1,34 @@
+package com.pedrocpacheco.aulao005.repositories;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.pedrocpacheco.aulao005.entities.Category;
+
+@Component
+public class CategoryRepository {
+    
+    // 1- Criando Map com relação ID -> Categoria
+    private Map<Long, Category> map = new HashMap<>();
+
+    // 2- Salvando em um Map a categoria referente ao seu ID
+    public void save(Category objCategory){
+        map.put(objCategory.getId(), objCategory); 
+    }
+
+    // 3- Retornando uma Categoria com o ID informado
+    public Category getById(Long id){
+        Category categoryFound = map.get(id);
+        return categoryFound;
+    }
+
+    // 4- Salvando os valores do Map em uma lista, e então retornando a lista.
+    public List<Category> getAll(){
+        return new ArrayList<Category>(map.values());
+    }
+
+}
