76895e9f567ae4568802421242e75201e7b8f4bc
pedrocpacheco <cpedropacheco@hotmail.com>
2023-05-06 10:13:25 -0300

    feat(app): implementing CommandLineRunner to autocreate categories

--- Aulões/aulao005/src/main/java/com/pedrocpacheco/aulao005/Aulao005Application.java
+++ Aulões/aulao005/src/main/java/com/pedrocpacheco/aulao005/Aulao005Application.java
@@ -1,13 +1,27 @@
 package com.pedrocpacheco.aulao005;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.pedrocpacheco.aulao005.entities.Category;
+import com.pedrocpacheco.aulao005.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class Aulao005Application {
+public class Aulao005Application implements CommandLineRunner{
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Aulao005Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		categoryRepository.save(new Category(1L, "Pedro"));
+		categoryRepository.save(new Category(2L, "Luane"));
 	}
 
 }
