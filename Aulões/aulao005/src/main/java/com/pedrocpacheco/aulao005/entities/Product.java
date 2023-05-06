2d92f0f0ca9ea5e795a3428fb72c66d7fe9facbd
pedrocpacheco <cpedropacheco@hotmail.com>
2023-05-06 10:22:05 -0300

    feat(Entities): Creating a Product Entity

---
+++ Aulões/aulao005/src/main/java/com/pedrocpacheco/aulao005/entities/Product.java
@@ -0,0 +1,11 @@
+package com.pedrocpacheco.aulao005.entities;
+
+public class Product {
+    
+    private Long id;
+    private String name;
+    private Double price;
+
+    private Category category;
+
+}
