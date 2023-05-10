22d2ff314c5ca14d75922820547cdccf133b4d09
pedrocpacheco <cpedropacheco@hotmail.com>
2023-05-09 21:06:53 -0300

    feat: Creating the Database for out Belonging

--- Intensivão Java Spring/dslist/src/main/java/com/pedrocpacheco/dslist/entities/Belonging.java
+++ Intensivão Java Spring/dslist/src/main/java/com/pedrocpacheco/dslist/entities/Belonging.java
@@ -1,7 +1,13 @@
 package com.pedrocpacheco.dslist.entities;
 
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 // Essa classe é justamente a relação de N para N, que vai ter seus
 // Proprios atributos (Vimos isso em Banco de Dados)
+@Entity
+@Table(name = "tb_belonging")
 public class Belonging {
     
     // Duas entidades que se relacionam
@@ -9,8 +15,63 @@ public class Belonging {
     //private GameList gameList;
 
     // Junção das duas Entidades antes feitas
+    @EmbeddedId // -> diz o ID é junto, das duas coisas dentro da classe.
     private BelongingPK id;
 
     private Integer position; // Atributo que surge justamente apos a relação
+
+    public Belonging(){
+
+    }
+
+    // Como o nosso ID precisa receber Game e GameList, passamos eles neste construtor
+    public Belonging(Game game, GameList list, Integer position){
+        this.id.setGame(game);
+        this.id.setGameList(list);
+        this.position = position;
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public void setId(BelongingPK id) {
+        this.id = id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Belonging other = (Belonging) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
 
 }
--- Intensivão Java Spring/dslist/src/main/java/com/pedrocpacheco/dslist/entities/BelongingPK.java
+++ Intensivão Java Spring/dslist/src/main/java/com/pedrocpacheco/dslist/entities/BelongingPK.java
@@ -1,10 +1,80 @@
 package com.pedrocpacheco.dslist.entities;
 
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 // Essa é a classe que vai unir as duas chaves das entidades que participam
 // Do Belonging. Numa nova tabela de N-N a PK é uma junção das duas chaves estrangeiras 
+@Embeddable // -> Diz que junta dois campos
 public class BelongingPK {
     
+    @ManyToOne
+    @JoinColumn(name = "game_id")
     private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "list_id")
     private GameList gameList;
+
+    public BelongingPK(){
+
+    }
+
+    public BelongingPK(Game game, GameList gameList) {
+        this.game = game;
+        this.gameList = gameList;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public GameList getGameList() {
+        return gameList;
+    }
+
+    public void setGameList(GameList gameList) {
+        this.gameList = gameList;
+    }
+
+    // Precisa comparar tanto o Game quanto a Lista:
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((game == null) ? 0 : game.hashCode());
+        result = prime * result + ((gameList == null) ? 0 : gameList.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        BelongingPK other = (BelongingPK) obj;
+        if (game == null) {
+            if (other.game != null)
+                return false;
+        } else if (!game.equals(other.game))
+            return false;
+        if (gameList == null) {
+            if (other.gameList != null)
+                return false;
+        } else if (!gameList.equals(other.gameList))
+            return false;
+        return true;
+    }
+
+    
 
 }
