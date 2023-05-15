15aeab4a92802914ca82dea55792e66eba0e3b6f
pedrocpacheco <cpedropacheco@hotmail.com>
2023-05-14 22:56:52 -0300

    feat: move method in GameRepository

--- Intensivão Java Spring/dslist/src/main/java/com/pedrocpacheco/dslist/services/GameListService.java
+++ Intensivão Java Spring/dslist/src/main/java/com/pedrocpacheco/dslist/services/GameListService.java
@@ -9,12 +9,16 @@ import org.springframework.transaction.annotation.Transactional;
 import com.pedrocpacheco.dslist.dto.GameListDTO;
 import com.pedrocpacheco.dslist.entities.GameList;
 import com.pedrocpacheco.dslist.repository.GameListRepository;
+import com.pedrocpacheco.dslist.repository.GameRepository;
 
 @Service
 public class GameListService {
     
     @Autowired
     private GameListRepository repository;
+
+    @Autowired
+    private GameRepository gameRepository;
 
     public List<GameList> findAll(){
         List<GameList> result = repository.findAll();
@@ -25,6 +29,10 @@ public class GameListService {
     public GameListDTO findById(Long id){
         GameListDTO result = new GameListDTO(repository.findById(id).get());
         return result;
+    }
+
+    public void move(Long id, int sourceIndex, int destinationIndex){
+        List<GameMinProjection> list = gameRepository.searchByList(id);
     }
 
 }
